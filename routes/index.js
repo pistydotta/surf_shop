95be2f889328a6ab61c2c3f3b2d8b65080a1a812
pistydotta <pisti_dotta@hotmail.com>
2020-07-31 15:06:09 -0300

    added users routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,44 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'Surf Shop - Home' });
 });
 
+router.get('/register', (req, res, next) => {
+  res.send('get /register')
+});
+
+router.post('/register', (req, res, next) => {
+  res.send('post /register')
+});
+
+router.get('/login', (req, res, next) => {
+  res.send('get /login')
+});
+
+router.post('/login', (req, res, next) => {
+  res.send('post /login')
+});
+
+router.get('/profile', (req, res, next) => {
+  res.send('get /profile')
+});
+
+router.put('/profile/:user_id', (req, res, next) => {
+  res.send('put /profile')
+});
+
+router.get('/forgot', (req, res, next) => {
+  res.send('get /forgot-password')
+});
+
+router.put('/forgot', (req, res, next) => {
+  res.send('put /forgot-password')
+});
+
+router.get('/reset/:token', (req, res, next) => {
+  res.send('get /reset-password')
+});
+
+router.put('/reset/:token', (req, res, next) => {
+  res.send('put /reset-password')
+});
+
 module.exports = router;
